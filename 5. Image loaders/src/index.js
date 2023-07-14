3c92c5aa4ba3689710c4d8786240b4f42c970ba3
Ashwini Kemshetty <21ashwinirao@gmail.com>
2023-07-14 23:45:15 +0530

    image loader and clean dir structure in dist config

---
+++ 5. Image loaders/src/index.js
@@ -0,0 +1,24 @@
+import style from './index.scss'
+import './clearButton'
+import _ from 'lodash'
+
+import logoImg from './assets/webpack_logo.png'
+
+const logo = document.getElementById('logo')
+
+logo.src = logoImg
+
+const button = document.getElementById('button');
+button.addEventListener('click',function (){
+    const header = document.getElementById('header');
+    header.innerHTML = 'Changed';
+
+    const items = ['A', 'B', 'C'];
+    const ul = document.getElementById('list');
+    _.forEach(items, (itemName)=>{
+        const item = document.createElement('li');
+        item.innerHTML = itemName;
+        ul.appendChild(item);
+    })
+})
+button.classList.add([style.button])
