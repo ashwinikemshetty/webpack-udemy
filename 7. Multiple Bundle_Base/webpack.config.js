dff6581c01dd91fa96081a8b44a31b52079b1e4c
Ashwini Kemshetty <21ashwinirao@gmail.com>
2023-07-15 00:34:25 +0530

    auto addition of js files to dist folder using plugin

--- 7. Multiple Bundle_Base/webpack.config.js
+++ 7. Multiple Bundle_Base/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
         index: './src/index.js',
@@ -7,5 +8,16 @@ module.exports = {
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [new htmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src/index.html'),
+        chunks: ['index'], // this index is from the index entry in the entry
+        inject: true,
+        filename: 'index.html'
+    }), new htmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src/products.html'),
+        chunks: ['products'], // this index is from the index entry in the entry
+        inject: true,
+        filename: 'products.html'
+    })]
 }
